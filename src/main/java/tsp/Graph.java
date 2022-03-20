81483548c9d95863b1c6249fb8c189e2a0179a4f
Dybol <mdyblik01@gmail.com>
2022-03-20 11:21:47 +0100

    Code refactor; make Graph abstract class

--- src/main/java/tsp/Graph.java
+++ src/main/java/tsp/Graph.java
@@ -1,14 +1,105 @@
 package tsp;
 
-public interface Graph {
-	double pathLength(Integer [] nodes);
-	boolean isPathCorrect(Integer [] nodes);
-	double PRD(Integer [] path);
-	double calculateDistance(int from, int to);
-	void printCurrentPath();
-	Integer[] getCurrentPath();
-	Integer[] getOptimalPath();
-	void setCurrentPath(Integer [] nodes);
-	void printMatrix();
-	int getNodesCount();
+import java.util.ArrayList;
+
+public abstract class Graph {
+
+	private final int size;
+
+	private Integer[] optimalPath = null;
+	private Integer[] currentPath = null;
+
+	public Graph(int size) {
+		this.size = size;
+	}
+
+	public abstract double calculateDistance(int from, int to);
+
+	public double pathLength(Integer[] nodes) {
+		double sum = 0.0;
+		if (isPathCorrect(nodes)) {
+			for (int i = 0; i < (size - 1); i++) {
+				sum += calculateDistance(nodes[i], nodes[i + 1]);
+			}
+			sum += calculateDistance(nodes[nodes.length - 1], nodes[0]);
+		} else {
+			sum = -1.0;
+		}
+		return sum;
+	}
+
+	public boolean isPathCorrect(Integer[] nodes) {
+		if (nodes.length != size) {
+			return false;
+		} else {
+			ArrayList<Integer> visited_points = new ArrayList<>();
+			for (Integer node : nodes) {
+				if (visited_points.contains(node)) {
+					return false;
+				} else if (node > size || node < 1) {
+					return false;
+				} else {
+					visited_points.add(node);
+				}
+			}
+			return true;
+		}
+	}
+
+	public double PRD(Integer[] path) {
+		double prd = -1.0;
+		if (isPathCorrect(path)) {
+			if (optimalPath != null) {
+				double optimal_length = pathLength(optimalPath);
+				//nie jestem pewny tego wzoru!
+				prd = ((pathLength(path) - optimal_length) / optimal_length) * 100.0;
+			} else {
+				System.out.println("Unknown optimal tour!");
+			}
+		} else {
+			System.out.println("Incorrect path!");
+		}
+		return prd;
+	}
+
+	public void printCurrentPath() {
+		for (Integer integer : currentPath) {
+			System.out.printf("%d\n", integer);
+		}
+	}
+
+	public void printMatrix() {
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				System.out.printf("%f ", calculateDistance(i, j));
+			}
+			System.out.print("\n");
+		}
+	}
+
+	public int getNodesCount() {
+		return size;
+	}
+
+	public Integer[] getCurrentPath() {
+		return currentPath;
+	}
+
+	public Integer[] getOptimalPath() {
+		return optimalPath;
+	}
+
+	public void setCurrentPath(Integer[] nodes) {
+		if (isPathCorrect(nodes))
+			this.currentPath = nodes;
+		else
+			System.out.println("Incorrect path!");
+	}
+
+	public void setOptimalPath(Integer[] nodes) {
+		if (isPathCorrect(nodes))
+			this.optimalPath = nodes;
+		else
+			System.out.println("Incorrect path!");
+	}
 }
--- src/main/java/tsp/matrix/model/MatrixGraph.java
+++ src/main/java/tsp/matrix/model/MatrixGraph.java
@@ -1,124 +1,20 @@
 package tsp.matrix.model;
 
-import java.util.ArrayList;
-
 import tsp.Graph;
 
 //Ta klasa zadziała zarówno dla symetrycznych i asymetrycznych grafów
-public class MatrixGraph implements Graph{
-
-	private final int size;
+public class MatrixGraph extends Graph{
 	
 	//konwencja: edges[from-1][to-1] jako krawędź od punktu <from> do punktu <to> 
-	private Integer [][] edges;
-	private Integer[] optimalPath = null;
-	private Integer[] currentPath = null;
+	private final Integer[][] edges;
 	
 	public MatrixGraph(Integer[][] cordTab) {
+		super(cordTab.length);
 		this.edges = cordTab;
-		this.size = cordTab.length;
-	}
-
-	public Integer[] getOptimalPath() {
-		return optimalPath;
-	}
-
-	public Integer[] getCurrentPath() {
-		return currentPath;
-	}
-	
-	public void setOptimalPath(Integer[] node_numbers) {
-		if(isPathCorrect(node_numbers))
-			this.optimalPath = node_numbers;
-		else
-			System.out.println("Incorrect path!");
-	}
-	
-	public void setCurrentPath(Integer[] node_numbers) {
-		if(isPathCorrect(node_numbers))
-			this.currentPath = node_numbers;
-		else
-			System.out.println("Incorrect path!");
 	}
 	
 	@Override
 	public double calculateDistance(int from, int to) {
 		return edges[from-1][to-1];
-	}
-	
-	@Override
-	public double pathLength(Integer[] node_numbers) {
-		double sum = 0.0;
-		if(isPathCorrect(node_numbers)) {
-			for (int i = 0; i < (size - 1); i++) {
-				sum += calculateDistance(node_numbers[i], node_numbers[i+1]);
-			}
-			sum += calculateDistance(node_numbers[node_numbers.length - 1], node_numbers[0]);
-		}
-		else {
-			sum = -1.0;
-		}
-		return sum;
-	}
-
-	@Override
-	public boolean isPathCorrect(Integer[] node_numbers) {
-		if(node_numbers.length != size) {
-			return false;
-		}
-		else {
-			ArrayList<Integer> visited_points = new ArrayList<>();
-			for(int i = 0; i<node_numbers.length; i++) {
-				if(visited_points.contains(node_numbers[i])) {
-					return false;
-				}
-				else if (node_numbers[i] > size || node_numbers[i] < 1){
-					return false;
-				}
-				else {
-					visited_points.add(node_numbers[i]);
-				}
-			}
-			return true;
-		}
-		
-	}
-
-	@Override
-	public double PRD(Integer[] path) {
-		double prd = -1.0;
-		if(isPathCorrect(path)) {
-			if(optimalPath != null) {
-				double optimal_length = pathLength(optimalPath);
-				//nie jestem pewny tego wzoru!
-				prd = ((pathLength(path) - optimal_length)/optimal_length) * 100.0;
-			}
-			else {
-				System.out.println("Unknown optimal tour!");
-			}
-		}
-		else {
-			System.out.println("Incorrect path!");
-		}
-		return prd;
-	}
-	
-	public void printMatrix() {
-		for(int i = 0; i < edges.length; i++) {
-			for(int j = 0; j < edges.length; j++) {
-				System.out.printf("%f ", calculateDistance(i,j));
-			}
-			System.out.print("\n");
-		}
-	}
-	
-	public void printCurrentPath() {
-		for(int i = 0; i < currentPath.length; i++) {
-			System.out.printf("%d\n", currentPath[0]);
-		}
-	}
-	
-	public int getNodesCount() {
-		return size;
 	}
 }
