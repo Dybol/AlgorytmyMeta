bb3a31f1f538c1a2246f1ce59a6def2fc4fe0ec5
Dybol <mdyblik01@gmail.com>
2022-03-21 19:50:53 +0100

    Add all instances

--- src/main/java/tsp/LoadData.java
+++ src/main/java/tsp/LoadData.java
@@ -3,43 +3,43 @@ package tsp;
 import tsp.algorithms.Algorithm2Opt;
 import tsp.algorithms.KRandomAlgorithm;
 import tsp.euc2d.Euc2dImporter;
+import tsp.euc2d.model.Euc2dGraph;
 import tsp.matrix.atsp.ATSPMatrixImporter;
 import tsp.matrix.model.MatrixGraph;
 import tsp.matrix.tsp.TSPMatrixImporter;
-import tsp.euc2d.model.Euc2dGraph;
 
 import java.io.FileNotFoundException;
 
 public class LoadData {
 	public static void main(String[] args) throws FileNotFoundException {
 		Euc2dImporter euc2dImporter = new Euc2dImporter();
-		euc2dImporter.importGraph("fl417.tsp");
+		euc2dImporter.importGraph("instances/fl417.tsp");
 
 		ATSPMatrixImporter ATSPMatrixImporter = new ATSPMatrixImporter();
-		ATSPMatrixImporter.importGraph("ft53.atsp");
+		ATSPMatrixImporter.importGraph("instances/ftv35.atsp");
 
 		TSPMatrixImporter tspMatrixImporter = new TSPMatrixImporter();
-		tspMatrixImporter.importGraph("bays29.tsp");
-		tspMatrixImporter.importOptimalTour("bays29.opt.tour");
+		tspMatrixImporter.importGraph("instances/bays29.tsp");
+		tspMatrixImporter.importOptimalTour("instances/bays29.opt.tour");
 
-		euc2dImporter.importGraph("pr76.tsp");
-		euc2dImporter.importOptimalTour("pr76.opt.tour");
+		euc2dImporter.importGraph("instances/pr76.tsp");
+		euc2dImporter.importOptimalTour("instances/pr76.opt.tour");
 
 		Euc2dGraph graph = euc2dImporter.getGraph();
 		System.out.println(graph.calculateDistance(2, 4));
-		
+
 		MatrixGraph matrixGraph = tspMatrixImporter.getGraph();
 		System.out.println(matrixGraph.calculateDistance(2, 4));
 
 		//przykładowa ścieżka 1->2->...->76->1
 		Integer[] path = new Integer[graph.getNodesCount()];
-		for(int i = 0; i < path.length; i++) {
-			path[i] = i+1;
+		for (int i = 0; i < path.length; i++) {
+			path[i] = i + 1;
 		}
-		
+
 		Integer[] path2 = new Integer[matrixGraph.getNodesCount()];
-		for(int i = 0; i < path2.length; i++) {
-			path2[i] = i+1;
+		for (int i = 0; i < path2.length; i++) {
+			path2[i] = i + 1;
 		}
 		System.out.printf("%f %%\n", graph.PRD(path));
 		System.out.printf("%f %%\n", matrixGraph.PRD(path2));
@@ -55,8 +55,8 @@ public class LoadData {
 		System.out.println("----------------------2OPT dla EUC2D----------------------------");
 
 		Integer[] tab = new Integer[76];
-		for(int i = 0; i <= 75; i++) {
-			tab[i] = i+1;
+		for (int i = 0; i <= 75; i++) {
+			tab[i] = i + 1;
 		}
 		graph.setCurrentPath(tab);
 		Algorithm2Opt alg = new Algorithm2Opt(graph);
@@ -66,8 +66,8 @@ public class LoadData {
 		System.out.println("----------------------2OPT dla Matrix----------------------------");
 
 		Integer[] tab2 = new Integer[matrixGraph.getNodesCount()];
-		for(int i = 0; i < matrixGraph.getNodesCount(); i++) {
-			tab2[i] = i+1;
+		for (int i = 0; i < matrixGraph.getNodesCount(); i++) {
+			tab2[i] = i + 1;
 		}
 
 		matrixGraph.setCurrentPath(tab2);
